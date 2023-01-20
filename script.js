c0ee4c585a74246d27bb281913790726c1ac38f0
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-01-20 18:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ function convertToRoman(num) {
     };
 
  // your code here
-    var roman = '',i;
-	for (i in obj) {
-    while ( num >= obj[i] ) {
-      roman +=i;
-      num -= obj[i];
+ //    var roman = '',i;
+	// for (i in obj) {
+ //    while ( num >= obj[i] ) {
+ //      roman +=i;
+ //      num -= obj[i];
+ //    }
+ //  }
+ //  return roman;
+	var roman = '';
+  for (var key in obj) {
+    if (num == obj[key]) {
+      return roman +=key;
+    }
+    var check = num > obj[key];
+    if(check) {
+      roman = roman + key.repeat(parseInt(num/obj[key]));
+      num = num%obj[key];
     }
   }
-  return roman;
+ return roman;
 
 
 
