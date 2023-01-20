8ec00bab50553abed23d2787e08a6302a9a6637d
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-01-20 14:10:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ function convertToRoman(num) {
     };
 
   //your code here
+  // var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+  for ( i in obj.length ) {
+    while ( num >= obj[i] ) {
+      roman += i;
+      num -= obj[i];
+    }
+  }
+  return roman;
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
