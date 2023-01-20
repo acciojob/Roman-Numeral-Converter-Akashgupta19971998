4f5f30f196c0720289c4bf8ff567e67b566e5a63
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-01-20 17:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,29 +11,27 @@ function convertToRoman(num) {
 
   //your code here
   // var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
- //  var roman = '',i;
-	// for (i in Object.keys(obj)) {
- //    while ( num >= obj[i] ) {
- //      roman += obj[i];
- //      num -= obj[i];
- //    }
- //  }
- //  return roman;
-
-	
-var roman='';
-if (num === 0) {
-return '';
-  }
-  for (let i in obj.length) {
-    if (num >= obj[i][0]) {
-      roman+= obj[i][1] + convertToRoman(num - obj[i][0]);
-		
+  var roman = '',i;
+	for (i in Object.keys(obj)) {
+    while ( num >= obj[i] ) {
+      roman += obj[i];
+      num -= obj[i];
     }
   }
-return roman;
+  return roman;
 
-
+	
+// var roman='';
+// if (num === 0) {
+// return '';
+//   }
+//   for (let i in obj.length) {
+//     if (num >= obj[i][0]) {
+//       roman+= obj[i][1] + convertToRoman(num - obj[i][0]);
+		
+//     }
+//   }
+// return roman;
 
 
 }
